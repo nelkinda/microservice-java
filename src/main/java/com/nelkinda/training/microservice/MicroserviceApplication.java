cfc937da9940bd6f54e3cdef1f0f964480dbcd59
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-03-04 23:22:47 +0530

    Resolve #7 by adding Jacoco, adding a rule that coverage must be 100%, and implementing the missing test-case to test main().

--- src/main/java/com/nelkinda/training/microservice/MicroserviceApplication.java
+++ src/main/java/com/nelkinda/training/microservice/MicroserviceApplication.java
@@ -2,11 +2,14 @@ package com.nelkinda.training.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 public class MicroserviceApplication {
+    static ApplicationContext appContext;
+
     public static void main(final String... args) {
-        SpringApplication.run(MicroserviceApplication.class, args);
+        appContext = SpringApplication.run(MicroserviceApplication.class, args);
     }
 }
--- src/test/java/com/nelkinda/training/microservice/MicroserviceApplicationTest.java
+++ src/test/java/com/nelkinda/training/microservice/MicroserviceApplicationTest.java
@@ -3,11 +3,21 @@ package com.nelkinda.training.microservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.nelkinda.training.microservice.MicroserviceApplication.appContext;
+import static com.nelkinda.training.microservice.MicroserviceApplication.main;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.SpringApplication.exit;
+
 @SpringBootTest
 class MicroserviceApplicationTest {
-
-    @SuppressWarnings({"PMD.UncommentedEmptyMethodBody", "PMD.JUnitTestsShouldIncludeAssert", "squid:S2699"})
+    @SuppressWarnings({"PMD.UncommentedEmptyMethodBody", "PMD.JUnitTestsShouldIncludeAssert", "squid:S2699", "java:S1186"})
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void mainRuns() {
+        main();
+        assertEquals(0, exit(appContext));
     }
 }
