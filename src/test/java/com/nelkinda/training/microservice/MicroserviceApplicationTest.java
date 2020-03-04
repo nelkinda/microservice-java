de878d26ee585e05dafaf9dae76864507637a249
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-03-04 14:52:25 +0530

    Resolves #3 by adding SonarLint to build.gradle.

--- src/test/java/com/nelkinda/training/microservice/MicroserviceApplicationTest.java
+++ src/test/java/com/nelkinda/training/microservice/MicroserviceApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MicroserviceApplicationTest {
 
-    @SuppressWarnings({"PMD.UncommentedEmptyMethodBody", "PMD.JUnitTestsShouldIncludeAssert"})
+    @SuppressWarnings({"PMD.UncommentedEmptyMethodBody", "PMD.JUnitTestsShouldIncludeAssert", "squid:S2699"})
     @Test
     void contextLoads() {
     }
