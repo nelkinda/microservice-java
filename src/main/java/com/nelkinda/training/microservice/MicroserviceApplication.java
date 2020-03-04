1a09310554ed217ea42b5cf39eff7c12da1df288
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-03-04 14:37:48 +0530

    Resolves #2 by adding PMD to build.gradle including corresponding configurations.

--- src/main/java/com/nelkinda/training/microservice/MicroserviceApplication.java
+++ src/main/java/com/nelkinda/training/microservice/MicroserviceApplication.java
@@ -3,6 +3,7 @@ package com.nelkinda.training.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 public class MicroserviceApplication {
     public static void main(final String... args) {
--- src/test/java/com/nelkinda/training/microservice/MicroserviceApplicationTest.java
+++ src/test/java/com/nelkinda/training/microservice/MicroserviceApplicationTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MicroserviceApplicationTest {
 
+    @SuppressWarnings({"PMD.UncommentedEmptyMethodBody", "PMD.JUnitTestsShouldIncludeAssert"})
     @Test
     void contextLoads() {
     }
